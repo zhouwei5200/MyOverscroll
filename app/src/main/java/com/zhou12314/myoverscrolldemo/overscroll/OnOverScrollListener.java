5681467948c82c9e6f5bb83b7182fe467f5ff3eb
unknown <>
2016-10-20 14:34:39 +0800

    fistcommit

---
+++ app/src/main/java/com/zhou12314/myoverscrolldemo/overscroll/OnOverScrollListener.java
@@ -0,0 +1,15 @@
+package com.zhou12314.myoverscrolldemo.overscroll;
+
+/**
+ * Created by changyou on 2015/11/11.
+ */
+public interface OnOverScrollListener {
+
+    void onTopOverScroll();
+
+    void onBottomOverScroll();
+
+    void onLeftOverScroll();
+
+    void onRightOverScroll();
+}
